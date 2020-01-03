5c2ea30580cd18ac993c74b2b01a36dbea80e509
Stephane-EQL <stephane.raes@gmail.com>
2020-01-03 12:08:45 +0100

    tests Stephane
    Création PageCreerCritere
    Script CRI_01

--- autom5/src/main/java/org/autom5/PageCritere.java
+++ autom5/src/main/java/org/autom5/PageCritere.java
@@ -1,5 +1,38 @@
 package org.autom5;
 
-public class PageCritere {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+
+public class PageCritere {
+	
+	@FindBy (xpath="//table[@id='eVDP_5-box']//td[@class='z-button-cm']")
+	WebElement btn_creer;
+	
+	@FindBy (xpath="//div[@class='clickable-rows z-grid']//div[(text()='Nom')]")
+	WebElement critère_nom;
+	
+	@FindBy (xpath="//div[@class='clickable-rows z-grid']//div[(text()='Code')]")
+	WebElement critère_code;
+	
+	@FindBy (xpath="//div[@class='clickable-rows z-grid']//div[(text()='Type')]")
+	WebElement critère_type;
+	
+	@FindBy (xpath="//div[@class='clickable-rows z-grid']//div[(text()='Activité')]")
+	WebElement critère_activite;
+	
+	@FindBy (xpath="//div[@class='clickable-rows z-grid']//div[(text()='Opérations')]")
+	WebElement critère_operations;	
+	
+	
+	
+	public PageCreerCritere clicBtnCreer(WebDriver driver) {
+		btn_creer.click();
+		return PageFactory.initElements(driver, PageCreerCritere.class);
+	}
+		
 }
+
+
