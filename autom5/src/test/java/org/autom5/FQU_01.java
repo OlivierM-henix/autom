a5f0b0bcc45bf596cdc3061f171ca4896b094a44
Olivier Monjauze <omonjauze@henix.fr>
2020-01-03 17:16:01 +0100

    MAJ canevasTest.java + creation et MAJ FQU_01.java

--- autom5/src/test/java/org/autom5/FQU_01.java
+++ autom5/src/test/java/org/autom5/FQU_01.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class canevasTest extends PageAbstract{
+public class FQU_01 extends PageAbstract{
 
 	WebDriver driver;
 
@@ -31,22 +31,26 @@ public class canevasTest extends PageAbstract{
 
 
 	@Test
-	public void test() {
-		//PAS 1 : Accéder à l’application et se connecter en tant que admin/admin
+	public void test() throws InterruptedException {
+		//PAS 1 Accéder à l’application et se connecter en tant que admin/admin
 		driver.get("http://localhost:8090/libreplan/");	
 		PageCnx pageCnx = PageFactory.initElements(driver, PageCnx.class);
 		PageIndex pageIndex = pageCnx.sidentifier("admin", "admin", driver);
 
-		//PAS 1 : Vérifier la bonne connexion
+		//PAS 1 Vérifier la bonne connexion
 		assertTrue(pageIndex.btn_deconnexion.isEnabled());
 		assertTrue(pageIndex.btn_calendrier.isEnabled());
 		assertEquals(pageIndex.txt_utilisateurConnecte.getText(),"utilisateur: admin");
+		
+		//PAS 2 Se rendre sur la page à tester l'aide de la méthode selectionnerMenu de la PageAbstract: exemple avec Ressources / Formulaires Qualité
 
-		// PAS 2 : Se rendre sur la page à tester l'aide de la méthode selectionnerMenu de la PageAbstract: exemple avec Ressources / Formulaires Qualité
-		// Adapter les noms à votre page !!!
 		pageIndex.selectionnerMenu(driver, "Ressources", "Formulaires qualité");
 		PageFormulaireQualite pageFormulaireQualite = PageFactory.initElements(driver, PageFormulaireQualite.class);
 	
 		//PAS 2 Vérifier la bonne connexion
+		assertTrue(pageFormulaireQualite.cln_nom.getText().equals("Nom"));
+		assertTrue(pageFormulaireQualite.cln_description.getText().equals("Description"));
+		assertTrue(pageFormulaireQualite.cln_operations.getText().equals("Opérations"));
+		Thread.sleep(5000);
 	}
 }
