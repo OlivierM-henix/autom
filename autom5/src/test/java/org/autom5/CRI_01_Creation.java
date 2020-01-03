1898dd714039be1c388a3cbd18301cb02d5062f9
Stephane-EQL <stephane.raes@gmail.com>
2020-01-03 12:28:56 +0100

    Update CRI_01_Creation.java
    script CRI_01

--- autom5/src/test/java/org/autom5/CRI_01_Creation.java
+++ autom5/src/test/java/org/autom5/CRI_01_Creation.java
@@ -2,19 +2,29 @@ package org.autom5;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 
 public class CRI_01_Creation {
 	
-	
+	WebDriver driver;
 	
 
+	//Before choix du navigateur 
+	
 	@Test
 	public void test() {
 		
 		
-		//driver outilTechnique choix du navigateur		
+		//choix du navigateur		
+		driver.get("http://localhost:8090/libreplan");
+		//Inserer Methode de connexion à la page index
+		
+		
+		//Inserer Methode de selection du menu : selectionnerMenu(driver,"Ressources","Criteres");
+		
+		
 		//PageCritere PageCritere = PageFactory.initElements(driver, PageCritere.class);
 		//assertEquals("Nom", PageCritere.critere_nom.getText());
 		//assertEquals("Nom", PageCritere.critere_code.getText());
