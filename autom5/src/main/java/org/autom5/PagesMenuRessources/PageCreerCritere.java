a2b27e3b3b9cc630f1969ac7125c1ef11fa79dca
Stephane-EQL <stephane.raes@gmail.com>
2020-01-03 16:54:44 +0100

    CRI_01 Stephane
    Deplacement des classes Main PageCritere et PageCreerCritere dans le package PageMenuRessources

--- autom5/src/main/java/org/autom5/PagesMenuRessources/PageCreerCritere.java
+++ autom5/src/main/java/org/autom5/PagesMenuRessources/PageCreerCritere.java
@@ -1,4 +1,4 @@
-package org.autom5;
+package org.autom5.PagesMenuRessources;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,27 +7,34 @@ import org.openqa.selenium.support.PageFactory;
 
 public class PageCreerCritere {
 	
-	@FindBy (xpath="//table[@id='eVDPh6-box']//td[@class='z-button-cm']")
+	@FindBy (xpath="//table[@id='lKcYh6-box']//td[@class='z-button-cm']")
+	public
 	WebElement btn_enregistrer;
-	
-	@FindBy (xpath="//table[@id='eVDPh6-box']//td[@class='z-button-cm']")
+			
+	@FindBy (xpath="//span[@class='saveandcontinue-button global-action z-button']")
+	public
 	WebElement btn_sauvegarder_continuer;
 	
-	@FindBy (xpath="//table[@id='eVDPh6-box']//td[@class='z-button-cm']")
-	WebElement btn_annuler;
+		@FindBy (xpath="//span[@class='cancel-button global-action z-button']")
+	public
+	WebElement btn_annuler;	
 	
 	@FindBy (xpath="//li[@id='eVDP45']//span[text()=\"Modifier\"]")
+	public
 	WebElement creer_critere_modifier;
 	
 	@FindBy (xpath="//input[@id='a35Xe5']")
+	public
 	WebElement champs_critere_nom;
 	
 	@FindBy (xpath="//textarea[@id='lKcYt5']")
+	public
 	WebElement champs_critere_description;
 	
 	// On constate que le code xml est différent pour 2 champs tetes de même nature : textarea et input
 	
 	@FindBy (xpath="//td[(text()='PARTICIPANT')]/ancestor::i/descendant::input']")
+	public
 	WebElement type_creer_critere;
 	
 	
--- autom5/src/test/java/org/autom5/CRI_01_Creation.java
+++ autom5/src/test/java/org/autom5/CRI_01_Creation.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.util.concurrent.TimeUnit;
 
+import org.autom5.PagesMenuRessources.PageCreerCritere;
+import org.autom5.PagesMenuRessources.PageCritere;
 import org.autom5.PagesMenuRessources.PageFormulaireQualite;
 import org.junit.After;
 import org.junit.Before;
@@ -12,14 +14,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-
-
-
 public class CRI_01_Creation {
 	
 	WebDriver driver;
 	
-
 	@Before
 	public void initialisations() throws InterruptedException {
 		driver = OutilTechnique.choisirNavigateur(ENavigateur.chrome);
@@ -27,11 +25,11 @@ public class CRI_01_Creation {
 	}
 
 
-	@After
+	/*@After
 	public void fermerNavigateur () {
 		driver.quit();
 	}
-
+	*/
 
 	@Test
 	public void test() throws InterruptedException {
@@ -47,27 +45,25 @@ public class CRI_01_Creation {
 
 		// Se rendre sur la page à tester l'aide de la méthode selectionnerMenu de la PageAbstract: exemple avec Ressources / Formulaires Qualité
 		// Adapter les noms à votre page !!!
-		pageIndex.selectionnerMenu(driver, "Ressources", "Formulaires qualité");
+		pageIndex.selectionnerMenu(driver, "Ressources", "Critère");
 		PageFormulaireQualite pageFormulaireQualite = PageFactory.initElements(driver, PageFormulaireQualite.class);
 		
 		
 		// Initialisation de la PageCritere : verification des champs dans le tableau et clic sur le bouton "Continuer"
 		PageCritere PageCritere = PageFactory.initElements(driver, PageCritere.class);
-		assertEquals("Nom", PageCritere.critere_nom.getText());
-		assertEquals("Code", PageCritere.critere_code.getText());
-		assertEquals("Type", PageCritere.critere_type.getText());
-		assertEquals("Activité", PageCritere.critere_activite.getText());
+		assertEquals("Nom", PageCritere.getCritere_nom().getText());
+		assertEquals("Code", PageCritere.getCritere_code().getText());
+		assertEquals("Type", PageCritere.getCritere_type().getText());
+		assertEquals("Activé", PageCritere.getCritere_active().getText());
 		assertEquals("Opérations", PageCritere.critere_operations.getText());
-		assertTrue(PageCritere.btn_creer.isEnabled());
-		
-		//Capture d'écran		
+		assertTrue(PageCritere.btn_creer.isDisplayed());
 		
 		//Capture d'écran - Ouverture de PageCreerCritere : vérifier que les boutons et champs sont présents dans la page
 		PageCreerCritere PageCreerCritere = PageCritere.clicBtnCreer(driver);
-		assertTrue(PageCreerCritere.btn_enregistrer.isEnabled());
-		assertTrue(PageCreerCritere.btn_sauvegarder_continuer.isEnabled());
-		assertTrue(PageCreerCritere.btn_annuler.isEnabled());
-		assertEquals("Modifier", PageCreerCritere.creer_critere_modifier.getText());
+		//assertTrue(PageCreerCritere.btn_enregistrer.isDisplayed());
+		//assertTrue(PageCreerCritere.btn_sauvegarder_continuer.isDisplayed());
+		//assertTrue(PageCreerCritere.btn_annuler.isDisplayed());
+		//assertEquals("Modifier", PageCreerCritere.creer_critere_modifier.getText());
 		//Capture d'écran - Capture pour le testeur de l'état du tableau proposé. A ajouter, les checkboxes qui doivent être cochées par défaut
 		
 		OutilTechnique.remplirChampTexte(PageCreerCritere.champs_critere_nom, "Critère - Test bouton [Annuler]");
