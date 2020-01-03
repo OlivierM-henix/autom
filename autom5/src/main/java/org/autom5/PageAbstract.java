92ccae213955eca83ada6165a434aa944fff510c
Kitoune <59090047+Kitoune@users.noreply.github.com>
2020-01-03 11:51:28 +0100

    Ajout de drivers web, mise à jour PageAbstract

--- autom5/src/main/java/org/autom5/PageAbstract.java
+++ autom5/src/main/java/org/autom5/PageAbstract.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public abstract class PageAbstract {
+	
 	public static void selectionnerMenu(WebDriver driver, String ogl,String btn) {
 		driver.findElement(By.xpath("//button[@class='z-menu-btn'][contains(text(),"+ogl+"]")).click();
 		driver.findElement(By.xpath("//button[@class='z-menu-item-cnt'][contains(text(),"+btn+"]")).click();
-	}
+	} // PAR EXEMPLE : selectionnerMenu(driver,"Ressources","Machines");
 }
