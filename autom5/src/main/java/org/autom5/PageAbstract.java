95d236793f51eafa85420c8b1eab89e6a3e8e776
Kitoune <59090047+Kitoune@users.noreply.github.com>
2020-01-03 11:44:39 +0100

    Ajout de dépendances PageAbstract

--- autom5/src/main/java/org/autom5/PageAbstract.java
+++ autom5/src/main/java/org/autom5/PageAbstract.java
@@ -1,10 +1,12 @@
 package org.autom5;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public abstract class PageAbstract {
-	public static <WebDriver> void selectionnerMenu(WebDriver driver, String ogl,String btn) {
-		WebElement a = driver.findElement(By.xpath("//button[@class='z-menu-btn'][contains(text(),"+ogl+"])");
-		WebElement b = driver.findElement(By.xpath("//button[@class='z-menu-item-cnt'][contains(text(),"+btn+"])");
+	public static void selectionnerMenu(WebDriver driver, String ogl,String btn) {
+		driver.findElement(By.xpath("//button[@class='z-menu-btn'][contains(text(),"+ogl+"]")).click();
+		driver.findElement(By.xpath("//button[@class='z-menu-item-cnt'][contains(text(),"+btn+"]")).click();
 	}
 }
