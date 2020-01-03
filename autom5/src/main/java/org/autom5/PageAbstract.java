2391397a7b0fa6e1278cd10f6257b816985408f9
Kitoune <59090047+Kitoune@users.noreply.github.com>
2020-01-03 16:46:51 +0100

    Mise à jour test

--- autom5/src/main/java/org/autom5/PageAbstract.java
+++ autom5/src/main/java/org/autom5/PageAbstract.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,9 +18,8 @@ public abstract class PageAbstract {
 	WebElement txt_utilisateurConnecte;
 
 	
-	public static void selectionnerMenu(WebDriver driver, String ogl,String btn) {
+	public void selectionnerMenu(WebDriver driver, String ogl,String btn) {
 		Actions action = new Actions (driver);
-		WebDriverWait wait = new WebDriverWait(driver, 20);
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
@@ -27,14 +27,20 @@ public abstract class PageAbstract {
 			e.printStackTrace();
 		}
 		WebElement b = driver.findElement(By.xpath("//button[@class='z-menu-btn'][contains(text(),'Ressources')]/../../../../.."));
-		wait.until(ExpectedConditions.elementToBeClickable(b));
 		
         action.moveToElement(b).build().perform();
-        
+        System.out.println("Je hover sur : "+b);
+        try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         WebElement a = driver.findElement(By.xpath("//a[@class='z-menu-item-cnt'][contains(text(),'"+btn+"')]"));
-        
-        wait.until(ExpectedConditions.elementToBeClickable(a));
+        System.out.println("Je clique sur : "+a);
         a.click();
+        
+//        return PageFactory.initElements(driver, PageAbstract.class);
 	} // PAR EXEMPLE : selectionnerMenu(driver,"Ressources","Machines");
 }
 
---
+++ autom5/src/main/java/org/autom5/PagesMenuRessources/PageAvancements.java
@@ -0,0 +1,7 @@
+package org.autom5.PagesMenuRessources;
+
+import org.autom5.PageAbstract;
+
+public class PageAvancements extends PageAbstract {
+
+}
