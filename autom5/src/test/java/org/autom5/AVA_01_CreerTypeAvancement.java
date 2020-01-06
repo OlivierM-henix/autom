b0c42afb9ca03636f17b8f316f3cd6358d279ec8
Kitoune <Kitoune@users.noreply.github.com>
2020-01-06 10:27:10 +0100

    AVA_01

--- autom5/src/test/java/org/autom5/AVA_01_CreerTypeAvancement.java
+++ autom5/src/test/java/org/autom5/AVA_01_CreerTypeAvancement.java
@@ -2,7 +2,6 @@ package org.autom5;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 
 import junit.framework.TestCase;
 
@@ -10,26 +9,18 @@ import java.util.concurrent.TimeUnit;
 
 import org.autom5.OutilTechnique;
 
-public class AppTestNikita extends TestCase {
+public class AVA_01_CreerTypeAvancement extends TestCase {
 	WebDriver driver;
 	ENavigateur chrome;
 	public void testApp(){
     	driver = OutilTechnique.choisirNavigateur(ENavigateur.chrome);
 		driver.get("http://localhost:8080/libreplan");
 		
-		new PageCnx().sidentifier (String login, String password, WebDriver driver);
-		
-		PageAbstract.selectionnerMenu(driver,"Configuration","Comptes utilisateurs");
+		PageAbstract.selectionnerMenu(driver,"Ressources","Types d'avancement");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//td[@class='z-button-cm'][.='Créer']/../../..")).click();
 		
-		// Parfois, popup d'erreur ! z-button-cm
-		driver.findElement(By.xpath("//td[@class='z-button-cm'][.='Continuer']")).click();
-		driver.findElement(By.xpath("//td[@class='z-button-cm'][.='Créer']/../../..")).click();
-		
-		OutilTechnique.remplirChampTexte(driver.findElement(By.xpath("//span[@class='z-label'][.=\"Nom d'utilisateur\"]/../../../td[2]/*/input")),"testAutomatisé");
-		OutilTechnique.remplirChampTexte(driver.findElement(By.xpath("//span[@class='z-label'][.=\"Mot de passe\"]/../../../td[2]/*/input")),"motdepasse");
-		OutilTechnique.remplirChampTexte(driver.findElement(By.xpath("//span[@class='z-label'][.=\"Confirmation du mot de passe\"]/../../../td[2]/*/input")),"motdepasse");
+		OutilTechnique.remplirChampTexte(driver.findElement(By.xpath("//span[@class='z-label'][.=\"Nom d'unité\"]/../../../td[2]/*/input")),"testAutomatisé");
 		
 		driver.findElement(By.xpath("//td[@class='z-button-cm'][.='Enregistrer']/../../..")).click();
 		
